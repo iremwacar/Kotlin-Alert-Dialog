50384d4bfa9b3bd5057caca3e735411486301cdb
irem acar <iremmacar35@gmail.com>
2024-10-09 12:18:43 +0300

    alert dialog

--- app/src/main/java/com/irem/androidalertdiolog/MainActivity.kt
+++ app/src/main/java/com/irem/androidalertdiolog/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.irem.androidalertdiolog
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.irem.androidalertdiolog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +30,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun kaydet(view: View){
+        val alert = AlertDialog.Builder(this@MainActivity)
+        alert.setTitle("Kayıt Et")
+        alert.setMessage("Kayıt etmek istediğinize emin misiniz?")
+        alert.setPositiveButton("Evet"){dialog,which->
+            Toast.makeText(this@MainActivity,"KAyıt edildi",Toast.LENGTH_LONG).show()
+        }
 
+        alert.setNegativeButton("Hayır",object :DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                Toast.makeText(this@MainActivity,"Kayıt iptal edildi!",Toast.LENGTH_LONG).show()
+            }
+        })
+        alert.show()
     }
 }
